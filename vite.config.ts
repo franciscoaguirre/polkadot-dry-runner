17b228f9bf832fa0a1f488f7e5bf8fb7f7d0bc80
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2024-09-13 11:48:40 +0200

    fix: use ESM

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import path from 'path';
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -17,13 +16,5 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
-  },
-  resolve: {
-    alias: [
-      {
-        find: /@polkadot-api\/descriptors/,
-        replacement: path.resolve(__dirname, 'node_modules', '@polkadot-api', 'descriptors'),
-      },
-    ],
   },
 });
